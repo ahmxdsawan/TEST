ff6cceaa373e61f250551dfc8a54976126d25a31
Ahmad Sawan <ahmad.sawan@outlook.com>
2024-11-13 22:57:15 -0500

    Delete backend directory

--- backend/src/main/java/com/poseidon/cis3760/spring/template/sewage/models/ContaminantExceedances.java
+++
@@ -1,6 +0,0 @@
-package com.poseidon.cis3760.spring.template.sewage.models;
-
-public interface ContaminantExceedances {
-    String getContaminant();
-    Integer getExceedances();
-}
--- backend/src/test/java/com/poseidon/cis3760/spring/template/TemplateApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.poseidon.cis3760.spring.template;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TemplateApplicationTests {
-
-}
