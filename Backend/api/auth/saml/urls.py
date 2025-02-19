3ed9d58f76014c568797ab62679db30d0d39d3a0
Ahmad Sawan <ahmad.sawan@outlook.com>
2025-02-19 09:47:27 -0500

    Delete Backend directory

--- Backend/api/auth/saml/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import microsoft_login, acs
-
-urlpatterns = [
-    path('login/', microsoft_login, name='microsoft-login'),
-    path('acs/', acs, name='saml-acs'),
-]
